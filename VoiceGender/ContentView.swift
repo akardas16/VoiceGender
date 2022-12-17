55d42c6bb49c7b5cef04aaaef985dee51d278034
akardas16 <akardas16@ku.edu.tr>
2022-12-18 00:20:43 +0300

    Initial Commit

---
+++ VoiceGender/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  VoiceGender
+//
+//  Created by Abdullah Kardas on 18.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VoiceGender/VoiceGenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceGenderApp.swift
+//  VoiceGender
+//
+//  Created by Abdullah Kardas on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct VoiceGenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
